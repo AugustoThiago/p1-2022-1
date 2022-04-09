6cd365e9c7bbf3a3202a0b37ab0f6fac01bf7f39
AugustoThiago <thiago.augusto.alvarenga@gmail.com>
2022-04-08 21:52:46 -0300

    2 files modified, 2 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,14 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { ThiagoComponent } from './thiago/thiago.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  imports:      [ BrowserModule, FormsModule, RouterModule.forRoot([
+    {path:'', component: ThiagoComponent},
+  ]) ],
+  declarations: [ AppComponent, HelloComponent, ThiagoComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
